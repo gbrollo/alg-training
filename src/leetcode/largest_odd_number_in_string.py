a30ec6640cc6109d8ed72eebd1bff95727ba1d25
gbrollo <giovanibrollo@gmail.com>
2024-05-04 17:23:14 -0300

    [04052024] largest-odd-number-in-string

---
+++ src/leetcode/largest_odd_number_in_string.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/boats-to-save-people/
+
+import unittest
+
+
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 != 0:
+                return num[: i + 1]
+
+        return ""
+
+
+param_list = [["5", "52"], ["", "4206"], ["35427", "35427"], ["1013389", "10133890"]]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        for expected, num in param_list:
+            with self.subTest():
+                self.assertEqual(expected, Solution().largestOddNumber(num))
+
+
+if __name__ == "__main__":
+    unittest.main()
